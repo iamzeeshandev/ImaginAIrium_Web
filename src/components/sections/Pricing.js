9b4c1c3dec4edbf2bc4b2424d0cdbc8380daea3b
Muhammad Zeeshan <iamzeeshan.dev@gmail.com>
2024-04-07 23:17:28 +0500

    Pricing button size updated:

--- src/components/sections/Pricing.js
+++ src/components/sections/Pricing.js
@@ -17,7 +17,7 @@ export default function Pricing() {
           <div className="col-lg-3 col-md-6 col-sm-12 pricing-block">
             <div className="pricing-block-two wow fadeInUp animated" data-wow-delay="00ms" data-wow-duration="1500ms">
               <div className="pricing-table">
-                <div className="table-header mb_40">
+                <div className="table-header mb_30">
                   <h3>AI Developer Hub</h3>
                   <p>For student, teacher and developer</p>
                   <h2>$4.99<span> /Month</span></h2>
@@ -36,11 +36,11 @@ export default function Pricing() {
           <div className="col-lg-3 col-md-6 col-sm-12 pricing-block">
             <div className="pricing-block-two active-block wow fadeInUp animated" data-wow-delay="200ms" data-wow-duration="1500ms">
               <div className="pricing-table">
-                <div className="table-header mb_40">
+                <div className="table-header mb_30">
                   <h3>ImaginAIrium</h3>
                   <p>For student, teacher and developer</p>
                   <h2>$9.99<span> /Month</span></h2>
-                  <Link href="index-3" className="theme-btn btn-one">Buy The Plan</Link>
+                  <Link href="index-3" className="theme-btn btn-one p_8">Buy The Plan</Link>
                 </div>
                 <div className="table-content">
                   <ul className="feature-list clearfix">
@@ -55,7 +55,7 @@ export default function Pricing() {
           <div className="col-lg-3 col-md-6 col-sm-12 pricing-block">
             <div className="pricing-block-two wow fadeInUp animated" data-wow-delay="400ms" data-wow-duration="1500ms">
               <div className="pricing-table">
-                <div className="table-header mb_40">
+                <div className="table-header mb_30">
                   <h3>VIP All Access</h3>
                   <p>For student, teacher and developer</p>
                   <h2>$19.99<span> /Month</span></h2>
@@ -74,7 +74,7 @@ export default function Pricing() {
           <div className="col-lg-3 col-md-6 col-sm-12 pricing-block">
             <div className="pricing-block-two wow fadeInUp animated" data-wow-delay="600ms" data-wow-duration="1500ms">
               <div className="pricing-table">
-                <div className="table-header mb_40">
+                <div className="table-header mb_30">
                   <h3>Enterprise</h3>
                   <p>For student, teacher and developer</p>
                   <h2>$499<span> /Month</span></h2>
